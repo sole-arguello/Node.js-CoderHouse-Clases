f7d98b30f72fc550129ff89c2aa3e34e83f653b4
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-08 21:34:02 -0300

    vinculo la ruta exportando de otra forma

--- clase-8/router-express/src/app.js
+++ clase-8/router-express/src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 
 import { usersRouter } from './routes/users.routes.js'
 import { petsRouter } from './routes/pets.routes.js'
+//otra forma de hacerlo
+import { orgsRouter } from './routes/orgs.routes.js'
 
 const port = 8080
 const app = express()
@@ -10,6 +12,8 @@ const app = express()
 app.use("/api/users", usersRouter)
 app.use("/api/pets", petsRouter)
 
+//otra forma de hacerlo
+app.use("/api/orgs", orgsRouter)
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}!`)
