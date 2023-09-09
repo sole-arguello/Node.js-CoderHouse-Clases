660af8ef12cacc58be05bef3fb29d962c144c0d6
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-08 21:27:56 -0300

    vinculo las rutas pets

--- clase-8/router-express/src/app.js
+++ clase-8/router-express/src/app.js
@@ -1,12 +1,14 @@
 import express from 'express'
 
 import { usersRouter } from './routes/users.routes.js'
+import { petsRouter } from './routes/pets.routes.js'
 
 const port = 8080
 const app = express()
 
 
 app.use("/api/users", usersRouter)
+app.use("/api/pets", petsRouter)
 
 
 app.listen(port, () => {
