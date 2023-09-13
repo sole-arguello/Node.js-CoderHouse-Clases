14bf5caef83394fd4ef929b9bb0c1461e46324e7
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-12 23:07:03 -0300

    agrego el milddware multer

--- clase-9/multer-lab/src/routes/pets.routes.js
+++ clase-9/multer-lab/src/routes/pets.routes.js
@@ -1,22 +1,40 @@
 import { Router } from "express";
+import { uploader } from "../utils.js";
 
 const router = Router();
 
 let pets = []
+//const userRole = 'user'
+const userRole = 'admin'
 
-router.get('/', (req, res) => {
+//middleware de enpoints
+const isAdmin = (req, res, next) => {
+    console.log('middleware de enpoints');
+    if(userRole === 'user'){
+        res.send('no tienes permiso')
+    }else{
+        req.isAdmin = true
+        next()
+    }
+}
+
+router.get('/', isAdmin, (req, res) => {
     res.json({data: pets})
 })
 
-router.post('/', (req, res) => {
+router.post('/',uploader.single('file'), (req, res) => {
+    //console.log('post pets', req);
     const petInfo = req.body
     if(!petInfo.name || !petInfo.age){
         res.json({message: 'faltan datos'})
-        return
+       // return
+    }else{
+        console.log("petsInfo", petInfo);
+        //console.log('req.file', req.file);
+        petInfo.thumbnail = req.file.filename
+        pets.push(petInfo)
+        res.json({message:'mascota creada'})
     }
-    console.log("petsInfo", petInfo);
-    pets.push(petInfo)
-    res.json({message:'mascota creada'})
 })
 
 
