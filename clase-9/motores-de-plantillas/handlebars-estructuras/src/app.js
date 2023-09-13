a685aa74f3b544d2c1e000ced10d7806bbe468d6
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-13 11:58:35 -0300

    Nuevo proyecto para actividad en clases

---
+++ clase-9/motores-de-plantillas/handlebars-estructuras/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { engine } from 'express-handlebars'
+import { __dirname } from './utils.js'
+import path from 'path'
+
+import { viewsRouter } from './routes/views.routes.js'
+
+const app = express()
+const port = 8080
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+})
+
+//configuracion del motor de plantillas
+app.engine('.hbs', engine({extname: '.hbs'}))//defino la extension de los archivos
+app.set('view engine', '.hbs')//defino el motor de plantillas
+//app.set('views', './views')//defino la ubicacion de los archivos(carpetas)
+app.set('views', path.join(__dirname, 'views'))//defino la ubicacion de los archivos(carpetas)
+
+//routes
+/*------------ Actividad en clases ------------------*/
+//no hace referencia a la ruta, sino al router utilizando el path que ayamos creado
+app.use(viewsRouter)
+
