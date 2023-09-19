dbdbc99080a8bf2e9bf1386e8ecbe73cd7a8098b
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-18 21:49:00 -0300

    configuracion y conexion al puerto

---
+++ clase-11/chat/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { __dirname } from "./utils.js";
+import path from "path";
+import { engine } from "express-handlebars";
+
+const port = 8080
+const app = express();
+
+//midleware
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.listen(port, () => {console.log(`Servidor escuchando en el puerto ${port}`)})
+
+//configuracion
+app.engine("hbs", engine({extname: ".hbs"}))
+app.set("view engine", "hbs")
+app.set("views", path.join(__dirname, "views"))
+
