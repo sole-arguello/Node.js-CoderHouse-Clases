1d7532ea321d90e6ecb82d1f939b828a4507fe1a
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-09-18 23:05:05 -0300

    capturo el usuario

--- clase-11/chat/src/public/js/home.js
+++ clase-11/chat/src/public/js/home.js
@@ -1,3 +1,22 @@
 console.log('javaScript en el frontend');
 //socket del cliente
 const socket = io();
+
+const userName = document.getElementById('userName')
+
+let user//variable para guardar el nombre del usuario
+Swal.fire({
+    title: 'chat',
+    text: 'Por favor ingrese su nombre de usuario',
+    input: 'text',
+    inputValidator: (value) => {
+        return !value && 'Debe ingresar el nombre de usuario para continuar'
+    },
+    allowOutsideClick: false,
+    allowEscapeKey: false
+}).then((inputValue) => {
+    console.log(inputValue);
+    user = inputValue.value
+    userName.innerHTML = user
+})
+    
