fbab7dd7f7b84e702d26c25a2cadf19c3f9b7e04
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-06 12:57:05 -0300

    mensaje para verificar coneccion en consola

--- clase-16/indexacion/src/config/db.Connection.js
+++ clase-16/indexacion/src/config/db.Connection.js
@@ -2,9 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://soledadar:g04D4zMd9O4y2GvK@cluster0.njbseut.mongodb.net/usersDB?retryWrites=true&w=majority', {
-            
-        })
+        await mongoose.connect('mongodb+srv://soledadar:g04D4zMd9O4y2GvK@cluster0.njbseut.mongodb.net/usersDB?retryWrites=true&w=majority')
+        console.log('Conectado a la base de datos')
     } catch (error) {
         console.error(`Hubo un error al conectar la base de datos ${error.message}`);
     }
