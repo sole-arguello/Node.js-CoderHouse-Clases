a0a28ea154681a2e367ebf40744924fe6bdadbda
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-06 15:05:01 -0300

    importar modelo

--- clase-16/indexacion/src/app.js
+++ clase-16/indexacion/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { connectDB } from './config/db.Connection.js';
+import { usersModel } from './models/users.model.js';
 
 const port = 8080;
 const app = express();
@@ -8,3 +9,12 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
 connectDB()
+
+app.get('/', async (req, res) => {
+    try {
+        const rsult = await usersModel.find({email: 'kaudeniscn@prlog.org'}).explain('executionStats')
+        res.json({status: 'success', data: rsult})
+    } catch (error) {
+        res.json({status: 'error', message: 'No se pudo hacer la consulta'})
+    }
+})
