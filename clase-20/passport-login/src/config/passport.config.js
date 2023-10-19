a179c0dbd05c27b46ec151ff6cf681f38b417ca9
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-19 20:18:28 -0300

    serializo y deserializo el usuario

--- clase-20/passport-login/src/config/passport.config.js
+++ clase-20/passport-login/src/config/passport.config.js
@@ -1,25 +1,25 @@
 import passport from "passport";
 
 import localStrategy from "passport-local";
-import { createHash, isValidPassword } from "../utils.js";
+import { createHash, inValidPassword } from "../utils.js";
 
-import {userModel } from "../persistence/mongo/managers/models.js"
+import { usersModel } from "../persistence/mongo/managers/models/users.models.js"
 
 
 export const initializePassport = () => {
     //estrategia para registrar los usuarios(primer parametro es un nombre de
     //estrategia que yo le quiera poner y el segundo la estrategia
-    passport.use('signUpLocalEstrat', new localStrategy(
+    passport.use('singUpLocalStrategy', new localStrategy(
         {
             //me permite trabajar con los datos del usuario
-            passReqCallback: true,
+            passReqToCallback: true,
             usernameField: 'email',//username ahora es igual email
         },
         async(req, username, password, done) =>{
 
             const {first_name} = req.body
             try {
-                const user = await userModel.findOne({email: username})
+                const user = await usersModel.findOne({email: username})
                 if(user){//null: que no hubo error, false: que no existe.
                     return done(null, false, {message: 'El usuario ya registrado'})
                 }
@@ -33,12 +33,22 @@ export const initializePassport = () => {
                 }
                 console.log(newUser)
                 //creo un nuevo objeto
-                const userCreated = await userModel.create(newUser)
+                const userCreated = await usersModel.create(newUser)
                 return done(null, userCreated)
             } catch (error) {
-                done(error)
+                return done(error)
             }
         }
     
     ))
+
+
+    passport.serializeUser((user, done)=>{
+        done(null, user._id)//id de la base de datos
+    })
+
+    passport.deserializeUser(async(id, done)=>{
+        const user = await usersModel.findById(id)
+        done(null, user)//queda guardado la info del ususario en una variable req.user
+    })
 }
