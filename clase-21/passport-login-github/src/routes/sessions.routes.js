6f1fd083387b2367b3decb1dc2f6954ae8182dc7
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-10-21 23:48:07 -0300

    creo la ruta para registro y callback github

--- clase-21/passport-login-github/src/routes/sessions.routes.js
+++ clase-21/passport-login-github/src/routes/sessions.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import passport from "passport";
-
+import { config } from "../config/config.js";
 
 //gestionar las sessiones
 const router = Router();
 
+//ruta de registro local
 router.post('/signUp', passport.authenticate('singUpLocalStrategy', {
     failureRedirect: '/api/sessions/fail-signup'}), async(req, res) => {
 
@@ -15,6 +16,16 @@ router.get('/fail-signup', (req, res) => {
     res.render('signUpViews', {error: 'No se pudo registrar el usuario'})
 })
 
+//ruta de registro github
+router.get('/singUpGithub', passport.authenticate('singUpGithubStrategy'))
+//ruta callback github
+router.get(config.github.callbackUrl, passport.authenticate('singUpGithubStrategy', {
+    failureRedirect: '/api/sessions/fail-signup'//fallo el registro 
+}), (req, res) => {
+    res.redirect('/profile')//ya esta logueado y registrado
+})
+
+//ruta de login 
 router.post('/login', passport.authenticate('loginLocalStrategy', {
     failureRedirect: 'api/sessions/fail-login',}),async(req, res) => {
 
